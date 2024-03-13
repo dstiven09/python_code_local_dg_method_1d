e440027e32ab8fe9e5e14e0a8d8fee447976fa37
dstiven09 <stivendf@gmail.com>
2024-03-13 22:54:45 +0100

    Update beji_battjes_numerical_parameters.py

--- model_adaptive_simulation/beji_battjes_numerical_parameters.py
+++ model_adaptive_simulation/beji_battjes_numerical_parameters.py
@@ -4,7 +4,7 @@ class BejiBattjesNumericalParameters:
         self.interpolation_order = 1
         self.max_iterations = 1000
         self.number_of_elements = 400
-        self.t_max = 40.0
+        self.t_max = 6.0
         self.dt = self.t_max / (self.number_of_elements - 1.)
         self.number_of_global_steps = 0
         self.discretization_order = 2
