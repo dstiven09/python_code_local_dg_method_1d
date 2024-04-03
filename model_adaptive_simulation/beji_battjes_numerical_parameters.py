0423907da24dcc52d9482991492a470dc55eeddf
dstiven09 <stivendf@gmail.com>
2024-04-03 18:55:55 +0200

    Update beji_battjes_numerical_parameters.py

--- model_adaptive_simulation/beji_battjes_numerical_parameters.py
+++ model_adaptive_simulation/beji_battjes_numerical_parameters.py
@@ -5,5 +5,5 @@ class BejiBattjesNumericalParameters:
         self.max_iterations = 10000
         self.number_of_elements = 400
         self.dt = 0.01 #(self.t_max) / (self.number_of_elements - 1.) #It needs revision
-        self.number_of_global_steps = 0
+        self.number_of_global_steps = 10
         self.discretization_order = 2
