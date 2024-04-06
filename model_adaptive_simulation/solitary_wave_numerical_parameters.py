8927f45eec27028aed405b737d50dc65b41123e2
dstiven09 <stivendf@gmail.com>
2024-04-06 22:01:06 +0200

    Update solitary_wave_numerical_parameters.py

--- model_adaptive_simulation/solitary_wave_numerical_parameters.py
+++ model_adaptive_simulation/solitary_wave_numerical_parameters.py
@@ -4,6 +4,6 @@ class SolitaryWaveNumericalParameters:
         self.interpolation_order = 1
         self.max_iterations = 10000
         self.number_of_elements = 400
-        self.dt = 0.05 #self.t_max / (self.number_of_elements - 1.)
+        self.dt = 0.01 #self.t_max / (self.number_of_elements - 1.)
         self.number_of_global_steps = 0
         self.discretization_order = 2
